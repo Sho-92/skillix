7d9264ebd0a5f923b38d160460f368686e01fcbd
Sho Maekawa <s.maekawa0509@gmail.com>
2024-12-23 02:08:59 +0900

    Fix database connection settings

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,20 @@
 <?php
+// 環境変数からJawsDBの接続URLを取得
+$cleardb_url = parse_url(getenv("JAWSDB_URL"));
+
 // DB接続設定
-$host = 'localhost';    // MySQLのホスト名
-$db   = 'synclyee';        // データベース名
-$user = 'root';         // MySQLのユーザー名（rootがデフォルト）
-$pass = 'root';             // パスワード（rootがデフォルト）
+$host = $cleardb_url["host"];   // Herokuのホスト
+$db   = ltrim($cleardb_url["path"], '/');   // Herokuのデータベース名（URLのパス部分がデータベース名）
+$user = $cleardb_url["user"];   // Herokuのユーザー名
+$pass = $cleardb_url["pass"];   // Herokuのパスワード
 $charset = 'utf8mb4';   // 文字セット（UTF-8を使う）
+
+// // DB接続設定(ローカルDB)
+// $host = 'localhost';    // MySQLのホスト名
+// $db   = 'synclyee';        // データベース名
+// $user = 'root';         // MySQLのユーザー名（rootがデフォルト）
+// $pass = 'root';             // パスワード（rootがデフォルト）
+// $charset = 'utf8mb4';   // 文字セット（UTF-8を使う）
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
